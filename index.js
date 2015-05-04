63703d04f758b099e788945ede0288699ddea5c5
Bálder Carraté <baldercm@gmail.com>
2015-05-04 22:06:06 +0200

    Fix configuration error

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 var express = require('express');
 var kraken = require('kraken-js');
-var database = require('./lib/database');
-var options = require('./lib/options');
+var options = require('./lib/options')();
 
+/*
+ * Create and configure application. Also exports application instance for use by tests.
+ * See https://github.com/krakenjs/kraken-js#options for additional configuration options.
+ */
 var app;
-
 
 app = module.exports = express();
 app.use(kraken(options));
--- test/server.e2e.js
+++ test/server.e2e.js
@@ -3,14 +3,14 @@
 var kraken = require('kraken-js');
 var express = require('express');
 var database = require('../lib/database');
-var options = require('../lib/options');
+var options = require('../lib/options')();
 
 function serverStartup(done) {
   var app = express();
   app.on('start', done);
   app.use(kraken({
     basedir: process.cwd(),
-    onconfig: options(app).onconfig
+    onconfig: options.onconfig
   }));
 
   return app.listen(1337);

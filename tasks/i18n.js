03071b690d2f04fbed1490a696a05378d2e97e78
Bálder Carraté <baldercm@gmail.com>
2015-05-06 01:54:21 +0200

    Remove localizr NPM dependency

--- tasks/i18n.js
+++ tasks/i18n.js
@@ -3,7 +3,7 @@
 
 module.exports = function clean(grunt) {
   // Load task
-  grunt.registerTask('i18n', [ 'clean', 'localizr', 'clean:tmp' ]);
+  grunt.registerTask('i18n', [ 'clean', 'clean:tmp' ]);
 
   // Options
   return {};
